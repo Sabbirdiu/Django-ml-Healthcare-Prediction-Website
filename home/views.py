f98e08e5cf217f14f84b244d65b865505420f43d
sabbirdiu <sabbir.s.dk7@gmail.com>
2020-12-22 00:17:09 +0600

    Added heart disease views

--- home/views.py
+++ home/views.py
@@ -112,3 +112,65 @@ def breast(request):
                       'context': value,
                       
                   })
+def heart(request):
+  
+    df = pd.read_csv('data/Heart_train.csv')
+    data = df.values
+    X = data[:, :-1]
+    Y = data[:, -1:]
+
+    value = ''
+
+    if request.method == 'POST':
+
+        age = float(request.POST['age'])
+        sex = float(request.POST['sex'])
+        cp = float(request.POST['cp'])
+        trestbps = float(request.POST['trestbps'])
+        chol = float(request.POST['chol'])
+        fbs = float(request.POST['fbs'])
+        restecg = float(request.POST['restecg'])
+        thalach = float(request.POST['thalach'])
+        exang = float(request.POST['exang'])
+        oldpeak = float(request.POST['oldpeak'])
+        slope = float(request.POST['slope'])
+        ca = float(request.POST['ca'])
+        thal = float(request.POST['thal'])
+
+        user_data = np.array(
+            (age,
+             sex,
+             cp,
+             trestbps,
+             chol,
+             fbs,
+             restecg,
+             thalach,
+             exang,
+             oldpeak,
+             slope,
+             ca,
+             thal)
+        ).reshape(1, 13)
+
+        rf = RandomForestClassifier(
+            n_estimators=16,
+            criterion='entropy',
+            max_depth=9
+        )
+
+        rf.fit(np.nan_to_num(X), Y)
+        rf.score(np.nan_to_num(X), Y)
+        predictions = rf.predict(user_data)
+
+        if int(predictions[0]) == 1:
+            value = 'have'
+        elif int(predictions[0]) == 0:
+            value = "don\'t have"
+
+    return render(request,
+                  'heart.html',
+                  {
+                      'context': value,
+                   
+                  })
