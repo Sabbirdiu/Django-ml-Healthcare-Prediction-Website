8e8874f0a728aa6f2a85626c96d82dd39b552b5a
sabbirdiu <sabbir.s.dk7@gmail.com>
2020-12-21 11:29:05 +0600

    Added urls path for breast cancer

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('',index,name='home'),
-    path('diabetes/',diabetes,name='diabetes')
+    path('diabetes/',diabetes,name='diabetes'),
+    path('breast/',breast,name='breast')
 ]
