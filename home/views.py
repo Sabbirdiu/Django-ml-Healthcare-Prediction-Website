a5bad71a59a8bad7ac985d6527af960489901ad2
sabbirdiu <sabbir.s.dk7@gmail.com>
2020-12-21 11:25:04 +0600

    Added views for breast cancer

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -59,3 +60,55 @@ def diabetes(request):
                       
                   }
                   )
+
+def breast(request):
+  
+    # Reading training data set. 
+
+    df = pd.read_csv('data/Breast_train.csv')
+    data = df.values
+    X = data[:, :-1]
+    Y = data[:, -1]
+    print(X.shape, Y.shape)
+
+  
+    # Reading data from user. 
+    
+    value = ''
+    if request.method == 'POST':
+
+        radius = float(request.POST['radius'])
+        texture = float(request.POST['texture'])
+        perimeter = float(request.POST['perimeter'])
+        area = float(request.POST['area'])
+        smoothness = float(request.POST['smoothness'])
+
+  
+        # Creating our training model. 
+        
+        rf = RandomForestClassifier(
+            n_estimators=16, criterion='entropy', max_depth=5)
+        rf.fit(np.nan_to_num(X), Y)
+
+        user_data = np.array(
+            (radius,
+             texture,
+             perimeter,
+             area,
+             smoothness)
+        ).reshape(1, 5)
+
+        predictions = rf.predict(user_data)
+        print(predictions)
+
+        if int(predictions[0]) == 1:
+            value = 'have'
+        elif int(predictions[0]) == 0:
+            value = "don\'t have"
+
+    return render(request,
+                  'breast.html',
+                  {
+                      'context': value,
+                      
+                  })
