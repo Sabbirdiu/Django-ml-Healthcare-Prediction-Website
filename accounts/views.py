c29f3ba80133275a0d95d9792b2c0c3492ff5595
sabbirdiu <sabbir.s.dk7@gmail.com>
2020-12-22 22:45:23 +0600

    Added patientsregistration views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+rom django.contrib import messages, auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from accounts.forms import *
+from accounts.models import User
 
-# Create your views here.
+class RegisterPatientView(CreateView):
+   
+    model = User
+    form_class = PatientRegistrationForm
+    template_name = 'accounts/patient/register.html'
+    success_url = '/'
+
+    extra_context = {
+        'title': 'Register'
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().dispatch(self.request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+
+        form = self.form_class(data=request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data.get("password1")
+            user.set_password(password)
+            user.save()
+            return redirect('accounts:login')
+        else:
+            return render(request, 'accounts/patient/register.html', {'form': form})
+
