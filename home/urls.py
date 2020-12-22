e9d4ae0f3bbc5364331b199da994b42f8190e8f5
sabbirdiu <sabbir.s.dk7@gmail.com>
2020-12-22 13:05:57 +0600

    Heart prediction is now ready for view

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('',index,name='home'),
     path('diabetes/',diabetes,name='diabetes'),
-    path('breast/',breast,name='breast')
+    path('breast/',breast,name='breast'),
+    path('heart/',heart,name='heart')
 ]
