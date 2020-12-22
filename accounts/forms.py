e190fe7fa56504048d287d991046cd85fa528089
sabbirdiu <sabbir.s.dk7@gmail.com>
2020-12-22 23:19:20 +0600

    Added user login form

--- accounts/forms.py
+++ accounts/forms.py
@@ -89,3 +89,35 @@ class PatientRegistrationForm(UserCreationForm):
         return user
 
 
+class UserLoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = None
+        self.fields['email'].widget.attrs.update({'placeholder': 'Enter Email'})
+        self.fields['password'].widget.attrs.update({'placeholder': 'Enter Password'})
+
+    def clean(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        if email and password:
+            self.user = authenticate(email=email, password=password)
+
+            if self.user is None:
+                raise forms.ValidationError("User Does Not Exist.")
+            if not self.user.check_password(password):
+                raise forms.ValidationError("Password Does not Match.")
+            if not self.user.is_active:
+                raise forms.ValidationError("User is not Active.")
+
+        return super(UserLoginForm, self).clean(*args, **kwargs)
+
+    def get_user(self):
+        return self.user
