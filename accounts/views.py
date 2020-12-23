a975ab86d68ece090ee456a29d188e2d3ecf01da
sabbirdiu <sabbir.s.dk7@gmail.com>
2020-12-23 11:35:17 +0600

    added patient edit form view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.contrib import messages, auth
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView,FormView,RedirectView
+from django.views.generic import CreateView,FormView,RedirectView,UpdateView
 from accounts.forms import *
 from accounts.models import User
+from .decorators import user_is_patient
+
+
 
 class RegisterPatientView(CreateView):
    
@@ -106,3 +109,29 @@ class RegisterDoctorView(CreateView):
         else:
             return render(request, 'accounts/doctor/register.html', {'form': form})
 
+class EditPatientProfileView(UpdateView):
+    model = User
+    form_class = PatientProfileUpdateForm
+    context_object_name = 'patient'
+    template_name = 'accounts/patient/edit-profile.html'
+    success_url = reverse_lazy('patient-profile-update')
+
+    @method_decorator(login_required(login_url=reverse_lazy('accounts:login')))
+    @method_decorator(user_is_patient)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(self.request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except Http404:
+            raise Http404("User doesn't exists")
+        # context = self.get_context_data(object=self.object)
+        return self.render_to_response(self.get_context_data())
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        print(obj)
+        if obj is None:
+            raise Http404("Patient doesn't exists")
+        return obj
