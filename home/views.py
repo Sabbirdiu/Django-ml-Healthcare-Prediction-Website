edfbe8b892c3b3d9119cce5259eb3cecb6535662
Ravi Tej <76604498+RaviTeja-12@users.noreply.github.com>
2023-07-11 00:42:42 +0530

    1st commit

--- home/views.py
+++ home/views.py
@@ -3,26 +3,28 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.impute import SimpleImputer
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
+
 def diabetes(request):
-    """ 
-    Reading the training data set. 
-    """
+    # Reading the training data set
     dfx = pd.read_csv('data/Diabetes_XTrain.csv')
     dfy = pd.read_csv('data/Diabetes_YTrain.csv')
     X = dfx.values
     Y = dfy.values
     Y = Y.reshape((-1,))
 
-    """ 
-    Reading data from user. 
-    """
+    # Reading data from user
     value = ''
-    if request.method == 'POST':
+    accuracy_rf = 0.0
+    accuracy_knn = 0.0
 
+    if request.method == 'POST':
         pregnancies = float(request.POST['pregnancies'])
         glucose = float(request.POST['glucose'])
         bloodpressure = float(request.POST['bloodpressure'])
@@ -43,52 +45,80 @@ def diabetes(request):
              age)
         ).reshape(1, 8)
 
-        knn = KNeighborsClassifier(n_neighbors=3)
-        knn.fit(X, Y)
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-        predictions = knn.predict(user_data)
+        # Random Forest Classifier
+        rf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=5)
+        rf.fit(X_train, y_train)
+        y_pred_rf = rf.predict(X_test)
+        accuracy_rf = accuracy_score(y_test, y_pred_rf)
+
+        # K-Nearest Neighbors Classifier
+        knn = KNeighborsClassifier(n_neighbors=3)
+        knn.fit(X_train, y_train)
+        y_pred_knn = knn.predict(X_test)
+        accuracy_knn = accuracy_score(y_test, y_pred_knn)
+
+        # Use the model with the highest accuracy for prediction
+        if accuracy_rf >= accuracy_knn:
+            predictions = rf.predict(user_data)
+        else:
+            predictions = knn.predict(user_data)
 
         if int(predictions[0]) == 1:
             value = 'Positive'
         elif int(predictions[0]) == 0:
             value = "Negative"
 
+        print("Random Forest Accuracy:", accuracy_rf*100)
+        print("K-Nearest Neighbors Accuracy:", accuracy_knn*100)
+
     return render(request,
                   'diabetes.html',
                   {
                       'context': value,
-                      
+                      'accuracy_rf': accuracy_rf,
+                      'accuracy_knn': accuracy_knn
                   }
-                  )
+                 )
+
 
 def breast(request):
-  
-    # Reading training data set. 
-
+    # Reading training data set
     df = pd.read_csv('data/Breast_train.csv')
     data = df.values
     X = data[:, :-1]
     Y = data[:, -1]
-    print(X.shape, Y.shape)
 
-  
-    # Reading data from user. 
-    
+    # Splitting the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+    # Reading data from user
     value = ''
-    if request.method == 'POST':
+    accuracy_rf = 0.0
+    accuracy_knn = 0.0
 
+    if request.method == 'POST':
         radius = float(request.POST['radius'])
         texture = float(request.POST['texture'])
         perimeter = float(request.POST['perimeter'])
         area = float(request.POST['area'])
         smoothness = float(request.POST['smoothness'])
 
-  
-        # Creating our training model. 
-        
-        rf = RandomForestClassifier(
-            n_estimators=16, criterion='entropy', max_depth=5)
-        rf.fit(np.nan_to_num(X), Y)
+        # Random Forest Classifier
+        rf = RandomForestClassifier(n_estimators=16, criterion='entropy', max_depth=5)
+        rf.fit(X_train, y_train)
+        y_pred_rf = rf.predict(X_test)
+        accuracy_rf = accuracy_score(y_test, y_pred_rf)
+        print("Accuracy of Random Forest Classifier:", accuracy_rf*100)
+
+
+        # K-Nearest Neighbors Classifier
+        knn = KNeighborsClassifier(n_neighbors=3)
+        knn.fit(X_train, y_train)
+        y_pred_knn = knn.predict(X_test)
+        accuracy_knn = accuracy_score(y_test, y_pred_knn)
+        print("Accuracy of K-Nearest Neighbors Classifier:", accuracy_knn*100)
 
         user_data = np.array(
             (radius,
@@ -98,31 +128,36 @@ def breast(request):
              smoothness)
         ).reshape(1, 5)
 
-        predictions = rf.predict(user_data)
-        print(predictions)
+        
+        # Use the model with the highest accuracy for prediction
+        if accuracy_rf >= accuracy_knn:
+            predictions = rf.predict(user_data)
+        else:
+            predictions = knn.predict(user_data)
 
         if int(predictions[0]) == 1:
             value = 'have'
         elif int(predictions[0]) == 0:
-            value = "don\'t have"
+            value = "don't have"
 
     return render(request,
                   'breast.html',
                   {
-                      'context': value,
-                      
-                  })
+                      'context': value
+                  }
+                 )
+
 def heart(request):
-  
     df = pd.read_csv('data/Heart_train.csv')
     data = df.values
     X = data[:, :-1]
     Y = data[:, -1:]
 
     value = ''
+    accuracy_rf = 0.0
+    accuracy_knn = 0.0
 
     if request.method == 'POST':
-
         age = float(request.POST['age'])
         sex = float(request.POST['sex'])
         cp = float(request.POST['cp'])
@@ -153,24 +188,43 @@ def heart(request):
              thal)
         ).reshape(1, 13)
 
-        rf = RandomForestClassifier(
-            n_estimators=16,
-            criterion='entropy',
-            max_depth=9
-        )
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-        rf.fit(np.nan_to_num(X), Y)
-        rf.score(np.nan_to_num(X), Y)
-        predictions = rf.predict(user_data)
+        # Impute missing values with mean
+        imputer = SimpleImputer(strategy='mean')
+        X_train_imputed = imputer.fit_transform(X_train)
+        X_test_imputed = imputer.transform(X_test)
+        user_data_imputed = imputer.transform(user_data)
+
+        # Random Forest Classifier
+        rf = RandomForestClassifier(n_estimators=16, criterion='entropy', max_depth=9)
+        rf.fit(X_train_imputed, y_train)
+        y_pred_rf = rf.predict(X_test_imputed)
+        accuracy_rf = accuracy_score(y_test, y_pred_rf)
+
+        # K-Nearest Neighbors Classifier
+        knn = KNeighborsClassifier(n_neighbors=3)
+        knn.fit(X_train_imputed, y_train)
+        y_pred_knn = knn.predict(X_test_imputed)
+        accuracy_knn = accuracy_score(y_test, y_pred_knn)
+        print("Accuracy of Random Forest Classifier:", accuracy_rf*100)
+        print("Accuracy of K-Nearest Neighbors Classifier:", accuracy_knn*100)
+
+        if accuracy_rf >= accuracy_knn:
+            predictions = rf.predict(user_data_imputed)
+        else:
+            predictions = knn.predict(user_data_imputed)
 
         if int(predictions[0]) == 1:
-            value = 'have'
+            value = 'positive'
         elif int(predictions[0]) == 0:
-            value = "don\'t have"
+            value = "negetive"
 
     return render(request,
                   'heart.html',
                   {
-                      'context': value,
-                   
-                  })
+                      'context': value
+                  }
+                 )
+
+
